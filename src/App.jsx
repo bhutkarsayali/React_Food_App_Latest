2ca7d786a72e92c1fe0634178e33b54b889ef12a
Sayali <bhutkarsayali@gmail.com>
2025-08-05 10:06:31 +0530

    Episode 7 - React router dom, basic routing with error element- installed react router dom, used createBrowserRouter for routing config, provide it with RouterProvider to the app and useRouteError hook and errorElement to show detailed errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 /**
  * <div id="parent">
  *  <div id ="child">
@@ -68,5 +72,22 @@ const AppLayout = () => {
     </div>
   );
 };
+
+//Router
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
