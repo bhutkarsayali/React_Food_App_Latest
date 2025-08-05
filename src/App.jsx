609abee661b9c9a890c2456b5a0aa35227535189
Sayali <bhutkarsayali@gmail.com>
2025-08-05 10:14:13 +0530

    Episode 7 - Created children routes using Outlet component and keeping Header intact

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
@@ -67,8 +67,9 @@ import Error from "./components/Error";
 const AppLayout = () => {
   return (
     <div className="container">
+      {/** Keep header inract and change below children routes */}
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
@@ -78,15 +79,21 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
     errorElement: <Error />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
